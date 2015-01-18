90df8d5bf78a89dcf05625ef8e7b59c6d136c1b5
Matthew G <mhguhit@gmail.com>
2015-01-18 02:17:49 -0800

    Changed text to see if ftploy works on live app.

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -4,18 +4,16 @@
 	<div id="main-story" class="w-350 padding-right float-left">
 		<img src="<?php echo $this->model->imagecache('college-gameday2.jpg', 350, 300); ?>" />
 		<h1>Test headline</h1>
-		<h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
+		<h2>This area should be used as a summary for the main story.</h2>
 	</div>
 	<div id="sec-story" class="w-200 padding-right float-left">
-		<p>test</p>
-	
-		<p>In a real application this could be the homepage.</p>
 		<?php foreach($newest as $new) {
 			echo '<li><p><a href="/article/getarticle/' . $new->article_url .'">' . $new->article_headline . '</a></p></li>';
 			echo "
 			";
 		}
 		?>
+		<p>This is a test sentence that is only meant to act as a place holder.</p>
 	</div>
 	<div class="clear"></div>
 	<h3 class="margin-mini-bottom">Another test</h3>
