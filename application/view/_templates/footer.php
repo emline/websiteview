cdc858972586f15fbc80c381d4ee1f6e3ae447e6
Matthew G <mhguhit@gmail.com>
2015-01-19 12:33:22 -0800

    Added forward slash after domain constant.

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -6,10 +6,10 @@
         <div id="tr" class="padding-last">
             <h3 class="color">Navigation</a></h3>
             <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>">Home</a></li>
-            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>about">About</a></li>
-            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>news">News</a></li>
-            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>stories">Stories</a></li>
-            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>contact">Contact</a></li>
+            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>/about">About</a></li>
+            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>/news">News</a></li>
+            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>/stories">Stories</a></li>
+            <li><a href="<?php echo URL_PROTOCOL . URL_DOMAIN; ?>/contact">Contact</a></li>
         </div>
         <div id="tr" class="padding-all">
             <h3 class="color">Recommended Links</h3>
