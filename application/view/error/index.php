3b48f9f9df6fa95b6f83e6b56c82bbfb6183766f
emline <matthewguhit@live.com>
2015-06-02 15:13:03 -0700

    Converted error page to grid system.

--- application/view/error/index.php
+++ application/view/error/index.php
@@ -1,5 +1,4 @@
-<div id="content">
-	<div class="front-content">
-		<h1>Page does not exist</h1>
-		<p>Make sure the URL you are trying to reach is correct.</p>
+	<div class="grid_8">
+		<h1>The page you're trying to load is probably buried somewhere else...</h1>
+		<p>Make sure the URL you are trying to reach is correct. If you still see this error page, feel free to contact me -- or send me a tweet.</p>
 	</div>
