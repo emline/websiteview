4e233dd8f212aaefd3542e9c9684c33439491387
Matthew G <mhguhit@gmail.com>
2015-07-03 14:09:59 -0700

    Minor changes to spacing

--- application/controller/search.php
+++ application/controller/search.php
@@ -3,7 +3,7 @@
 //$articleID = filter_input(INPUT_GET, 'aid', FILTER_SANITIZE_SPECIAL_CHARS);
 
 class Search extends Controller {
-	
+
     public function index()
     {
 
@@ -13,36 +13,39 @@ class Search extends Controller {
         require APP . 'view/_templates/sidebar.php';
         require APP . 'view/_templates/footer.php';
     }
-	
-	public function getKeyword() {
-	
-            $keyword = filter_input(INPUT_GET,'q');
-	
-            $newest = $this->model->getKeyword($keyword);
-			
-			//php-pagination library include
-            require APP . 'libs/php-pagination/Pagination.class.php';
-			
-			// determine page (based on <_GET>)
-			$page = isset($_GET['page']) ? ((int) $_GET['page']) : 1;
 
-			// instantiate; set current page; set number of records
-			// See: http://web.onassar.com/blog/2012/06/09/github-project-php-pagination/
-			$pagination = (new Pagination());
-			$pagination->setCurrent($page);
-			$pagination->setCrumbs(4);
-			$pagination->setTotal(200);
+	   public function getKeyword()
+     {
 
-			// grab rendered/parsed pagination markup
-			$markup = $pagination->parse();
-			
-			
-			// Core Includes
-            require APP . 'view/_templates/header.php';
-            require APP . 'view/search/index.php';
-            require APP . 'view/_templates/sidebar.php';
-            require APP . 'view/_templates/footer.php';
-	
+        // Defines input from search action.
+        $keyword = filter_input(INPUT_GET,'q');
+
+        // Object to be used in view.
+        $newest = $this->model->getKeyword($keyword);
+
+  			// php-pagination library include
+        require APP . 'libs/php-pagination/Pagination.class.php';
+
+  			// determine page (based on <_GET>)
+  			$page = isset($_GET['page']) ? ((int) $_GET['page']) : 1;
+
+  			// instantiate; set current page; set number of records
+  			// See: http://web.onassar.com/blog/2012/06/09/github-project-php-pagination/
+  			$pagination = (new Pagination());
+  			$pagination->setCurrent($page);
+  			$pagination->setCrumbs(4);
+  			$pagination->setTotal(200);
+
+  			// grab rendered/parsed pagination markup
+  			$markup = $pagination->parse();
+
+
+  			// Core Includes
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/search/index.php';
+        require APP . 'view/_templates/sidebar.php';
+        require APP . 'view/_templates/footer.php';
+
 	}
 
 }
