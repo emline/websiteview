7972c3c06f8b2ca038765ac77dc9573a48fa10e5
geek3899 <geek3899@p3nlhftpg238.shr.prod.phx3.secureserver.net>
2015-07-14 01:46:13 -0700

    Initial commit

---
+++ application/controller/backend.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Backend extends Controller {
+	
+	public function index()
+	{
+		
+		
+		require APP . 'view/backend/addarticle.php';
+		
+		
+	}
+	
+	
+	public function addArticle() 
+	{
+
+		require APP . 'view/backend/addarticle.php';	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ application/view/backend/addarticle.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Add Article</title>
+	</head>
+	
+	<body>
+		<h1>
+			Add Article
+		</h1>
+		<form action="/backend/addarticle/">
+			<input type="article_title" />
+		</form>
+		<input 
+	</body>
+	
+</html>
