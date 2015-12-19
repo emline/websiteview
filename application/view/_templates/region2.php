e68803ea9702f0efd5e543127a1c9496307f3b7f
Matt <noemail>
2015-12-19 11:21:41 -0700

    Stopped using ad300-250 div in home and article views.

--- application/view/_templates/region2.php
+++ application/view/_templates/region2.php
@@ -1,5 +1,7 @@
   <div class="grid_4">
-    <div class="ad300x250"><a href="http://www.redcross.org/templates/render/render.jsp?pageId=3400006&scode=RSG00000E020&subcode=RCO_Storytellers_WebBanner"><img src="http://www.redcross.org:80/images/MEDIA_CustomProductCatalog/m16141464_300x250-Storytellers-Angelina-General-Web-Banner.jpg" alt="Storytellers Web Banner" /></a></div>
+    <div style="height: 250px; margin: 0 0 20px 0; width: 300px;">
+        <a href="http://www.redcross.org/templates/render/render.jsp?pageId=3400006&scode=RSG00000E020&subcode=RCO_Storytellers_WebBanner"><img src="http://www.redcross.org:80/images/MEDIA_CustomProductCatalog/m16141464_300x250-Storytellers-Angelina-General-Web-Banner.jpg" alt="Storytellers Web Banner" /></a>
+    </div>
 
     <div class="blk">
     <h3 class="blk-header">Newest
