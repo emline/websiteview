380e6a0aba968b10738a63fedc30cda8a91e029f
Matt <noemail>
2016-01-06 14:36:06 -0700

    Fixed search page to show results.

--- application/view/search/index.php
+++ application/view/search/index.php
@@ -1,6 +1,14 @@
   <div class="grid_8">
-      <h2>Not Available2</h2>
+      <h2>Search Results</h2>
 
 	   
-	   <?php echo $markup; ?>
+	   <?php 
+	     foreach ($keyword as $v) {
+	     	
+	      echo '<li><p><a href="/article/getarticle/' . $v->article_url . '">' . $v->article_headline . '</a><br /></p></li>';
+	       	     	
+	     }
+ 
+	   
+	   ?>
   </div>
