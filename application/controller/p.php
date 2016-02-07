0873558cc7b206c84737d6a94233797a234d7d8c
Matt <noemail>
2016-02-07 12:00:52 -0700

    Added category method to load category pages with new URI.

--- application/controller/p.php
+++ application/controller/p.php
@@ -50,5 +50,20 @@ class P extends Controller {
 		require APP . 'view/page/' . $param . '.php';
 		require APP . 'view/_templates/footer.php';
 	}
+	
+	 public function category($param)
+	 {
+	 	
+		$category = $this->model->getCategory($param);
+		$categoryArray = (array) $category;
+		$newest = $this->model->getNewest();
+
+        require APP . 'view/_templates/header.php';
+        //require APP . 'view/_templates/carousel.php';
+        require APP . 'view/categories/' . $param . '.php';
+        require APP . 'view/_templates/region2.php';
+        require APP . 'view/_templates/footer.php';
+
+	 }
 
 }
